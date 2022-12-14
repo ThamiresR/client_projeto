f31037e8fae30bd753fd5ef8db2ca59724b8611d
ThamiresR <thamiresvessani@gmail.com>
2022-12-14 15:05:52 -0300

    paginas adicionadas

---
+++ pages/addProjeto.vue
@@ -0,0 +1,43 @@
+<template>
+    <main class="container my-5">
+        <div class="row">
+            <div class="col-12 text-center my-3">
+                <h2 class="mb-3 display-4 text-uppercase">Adicionar Projeto</h2>
+        </div>
+
+        <div class="col-12 text-center my-3 ">
+            <form>
+                <div class="form-group">
+                    <label >Titulo</label>
+                    <input type="text" class="form-control">
+                </div>
+                <div class="form-group">
+                    <label >Descricao</label>
+                    <input type="text" class="form-control">
+                </div>
+                <div class="form-group">
+                    <label >Data Inicio</label>
+                    <input type="date" class="form-control">
+                </div>
+                <div class="form-group">
+                    <label >Data Fim</label>
+                    <input type="date" class="form-control">
+                </div>
+                <div class="form-group">
+                    <label for>Coordenador</label>
+                    <select class="form-control">
+                        <option value="Easy">Easy</option>
+                        
+                    </select>
+                </div>
+                <button type="submit" class="btn btn-primary">Adicionar</button>
+            </form>
+        </div>
+        </div>
+    </main>
+</template>
+<script>
+
+</script>
+<style scoped>
+</style>
---
+++ pages/reservar.vue
@@ -0,0 +1,46 @@
+<template>
+    <main class="container my-5">
+        <div class="row">
+            <div class="col-12 text-center my-3">
+                <h2 class="mb-3 display-4 text-uppercase">Adicionar Projeto</h2>
+        </div>
+
+        <div class="col-12 text-center my-3 ">
+            <form>
+                <div class="form-group">
+                    <label >Data</label>
+                    <input type="date" class="form-control">
+                </div>
+                
+                <div class="form-group">
+                    <label for>Horario</label>
+                    <select class="form-control">
+                        <option value="Easy">Easy</option>
+                       
+                    </select>
+                </div>
+                <div class="form-group">
+                    <label for>Computador</label>
+                    <select class="form-control">
+                        <option value="Easy">Easy</option>
+                        
+                    </select>
+                </div>
+                <div class="form-group">
+                    <label for>Aluno</label>
+                    <select class="form-control">
+                        <option value="Easy">Easy</option>
+                       
+                    </select>
+                </div>
+                <button type="submit" class="btn btn-primary">Adicionar</button>
+            </form>
+        </div>
+        </div>
+    </main>
+</template>
+<script>
+
+</script>
+<style scoped>
+</style>
