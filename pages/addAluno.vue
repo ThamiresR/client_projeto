0ffe3240bc607f8f858c9372523b89bda9779b4d
ThamiresR <thamiresvessani@gmail.com>
2022-12-14 22:54:20 -0300

    cadastro de aluno

--- pages/addAluno.vue
+++ pages/addAluno.vue
@@ -6,22 +6,22 @@
         </div>
 
         <div class="col-12 text-center my-3 ">
-            <form>
+            <form @submit.prevent="adicionaAluno">
                 <div class="form-group">
                     <label >Nome</label>
-                    <input type="text" class="form-control">
+                    <input type="text" class="form-control" v-model="aluno.name">
                 </div>
                 <div class="form-group">
                     <label >Email</label>
-                    <input type="mail" class="form-control">
+                    <input type="mail" class="form-control" v-model="aluno.email">
                 </div>
                 <div class="form-group">
                     <label >Login</label>
-                    <input type="text" class="form-control">
+                    <input type="text" class="form-control" v-model="aluno.login">
                 </div>
                 <div class="form-group">
                     <label>Senha</label>
-                    <input type="password" class="form-control">
+                    <input type="password" class="form-control" v-model="aluno.senha">
                 </div>
                 <button type="submit" class="btn btn-primary">Entrar</button>
             </form>
@@ -30,7 +30,42 @@
     </main>
 </template>
 <script>
-
+export default {
+    head(){
+        return{
+            title: "Adicionar novo aluno"
+        };
+    },
+    data(){
+        return{
+            aluno:{
+                name:"",
+                email: "",
+                login: "",
+                senha: "",
+            }         
+        };
+    },
+    methods:{
+        async adicionaAluno(){
+            const config = {
+                headers: {"content-type": "multipart/form-data"}
+            };
+            let formData = new FormData();
+            for (let data in this.aluno){
+                formData.append(data, this.aluno[data]);
+            }
+            
+            try{
+                let response = await this.$axios.$post("/aluno/", formData, config);
+                this.$router.push("/coordenador");
+                return alert("Novo aluno cadastrado!");
+            }catch(e){
+                console.log(e);
+            }
+        }
+    }
+}
 </script>
 <style scoped>
 </style>
